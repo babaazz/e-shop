95e40da8e5d3041d545ce2907ae4954ef9dbfff5
Sunil Chauhan <sunil121ml@gmail.com>
2022-08-13 18:47:45 +0530

    adding cartReducer in cart context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useReducer } from "react";
 
 const addItem = (cartItems, itemToAdd) => {
   const itemAlreadyExistInCart = cartItems.find(
@@ -31,39 +31,73 @@ const clearItem = (cartItems, itemToRemove) => {
 };
 
 export const CartContext = createContext({
-  isCartOpen: false,
+  isCartOpen: true,
   setIsCartOpen: () => null,
   cartItems: [],
   addItemToCart: () => null,
+  removeItemFromCart: () => null,
   cartTotal: 0,
   clearItemFromCheckout: () => null,
 });
 
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartTotal: 0,
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "SET_CART_ITEMS":
+      return {
+        ...state,
+        ...payload,
+      };
+    case "TOGGLE_CART":
+      return {
+        ...state,
+        ...payload,
+      };
+    default:
+      throw new Error(`unhandled type ${type} in cartReducer`);
+  }
+};
+
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [{ cartItems, cartTotal, isCartOpen }, dispatch] = useReducer(
+    cartReducer,
+    INITIAL_STATE
+  );
 
-  const [cartItems, setCartItems] = useState([]);
+  const setIsCartOpen = (bool) => {
+    dispatch({ type: "TOGGLE_CART", payload: { isCartOpen: bool } });
+  };
 
-  const [cartTotal, setCartTotal] = useState(0);
-
-  useEffect(() => {
-    const newTotal = cartItems.reduce(
+  const updateCardItems = (newCartItems) => {
+    const newTotal = newCartItems.reduce(
       (total, item) => total + item.quantity,
       0
     );
-    setCartTotal(newTotal);
-  }, [cartItems]);
+    dispatch({
+      type: "SET_CART_ITEMS",
+      payload: { cartItems: newCartItems, cartTotal: newTotal },
+    });
+  };
 
   const addItemToCart = (itemToAdd) => {
-    setCartItems(addItem(cartItems, itemToAdd));
+    const newCartItems = addItem(cartItems, itemToAdd);
+    updateCardItems(newCartItems);
   };
 
   const removeItemFromCart = (itemToRemove) => {
-    setCartItems(removeItem(cartItems, itemToRemove));
+    const newCartItems = removeItem(cartItems, itemToRemove);
+    updateCardItems(newCartItems);
   };
 
   const clearItemFromCheckout = (itemToRemove) => {
-    setCartItems(clearItem(cartItems, itemToRemove));
+    const newCartItems = clearItem(cartItems, itemToRemove);
+    updateCardItems(newCartItems);
   };
 
   const value = {
